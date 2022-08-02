769d12390ca204558c2ad90e7dd8a2a2d9caf8b3
AndersonRMac <anderson.ramosmaciel@gmail.com>
2022-08-02 16:53:11 -0300

    depuração 02-08-2022

--- new/src/main/java/com/example/demo/repository/EntradasRepository.java
+++ new/src/main/java/com/example/demo/repository/EntradasRepository.java
@@ -3,7 +3,6 @@ package com.example.demo.repository;
 import com.example.demo.model.Entradas;
 import org.springframework.data.repository.CrudRepository;
 
-import javax.persistence.Id;
 
 public interface EntradasRepository extends CrudRepository<Entradas, Long> {
 }
--- new/src/main/java/com/example/demo/repository/UsuarioRepostory.java
+++ new/src/main/java/com/example/demo/repository/UsuarioRepostory.java
@@ -3,7 +3,6 @@ package com.example.demo.repository;
 import com.example.demo.model.Usuario;
 import org.springframework.data.repository.CrudRepository;
 
-import javax.persistence.Id;
 
 public interface UsuarioRepostory extends CrudRepository<Usuario, Long> {
 }
